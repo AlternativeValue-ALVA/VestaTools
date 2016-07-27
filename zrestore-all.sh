f4a40d9f1ce0bc1b82934587fa4c2925e603c31e
Joshua Bayfield <webmaster@gwiddle.co.uk>
2016-07-27 12:51:32 +0100

    Add zrestore-all.sh

---
+++ zrestore-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SYNTAX - zrestore-all.sh
+#NOTE: This script should be located in /backup to work correctly.
+#Restores all user backup files in /backup. Helpful for server moves.
+
+for X in [T-Z]*; do
+    user=${X::-15}
+    echo $X
+    rm -rf /backup/tmp.*
+    /usr/local/vesta/bin/v-restore-user $user $X
+done
